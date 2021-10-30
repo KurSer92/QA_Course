2e67a789b70754f4e45d157e4b2ce99a2ed08d6f
Sergei Kurochkin <KurSer92@gmail.com>
2021-10-30 20:26:21 +0300

    edit SQL_HW_2

--- SQL/Sergei_Kurochkin_SQL_HomeWork_2.sql
+++ SQL/Sergei_Kurochkin_SQL_HomeWork_2.sql
@@ -1,4 +1,4 @@
---1)Создать таблицу employees
+--1) Создать таблицу employees
 --- id. serial,  primary key,
 --- employee_name. Varchar(50), not null
 
@@ -6,13 +6,13 @@ create table employees (
 	id serial primary key,
 	employee_name varchar(50) not null);
 
---2)Наполнить таблицу employee 70 строками.
+--2) Наполнить таблицу employee 70 строками.
 insert into employees (id, employee_name)
 values (default, 'Laura Thompson');
 
 select * from employees e 
 
---3)Создать таблицу salary
+--3) Создать таблицу salary
 --- id. Serial  primary key,
 --- monthly_salary. Int, not null
 
@@ -20,13 +20,13 @@ create table salary (
 	id serial primary key,
 	monthly_salary int not null);
 
---4)Наполнить таблицу salary 15 строками: от 1000 до 1500:
+--4) Наполнить таблицу salary 15 строками: от 1000 до 1500:
 insert into salary (id, monthly_salary)
 values (default, 2500);
 
 select * from salary
 
---5)Создать таблицу employee_salary
+--5) Создать таблицу employee_salary
 --- id. Serial  primary key,
 --- employee_id. Int, not null, unique
 --- salary_id. Int, not null
@@ -36,7 +36,7 @@ create table employee_salary (
 	employee_id int not null unique,
 	salary_id int not null);
 
---6)Наполнить таблицу salary 40 строками:
+--6) Наполнить таблицу salary 40 строками:
 -- в 10 строк из 40 вставить несуществующие employee_id
 
 insert into employee_salary (id, employee_id, salary_id)
@@ -44,7 +44,7 @@ values (default, 51, 14);
 
 select * from employee_salary es 
 
---7)Создать таблицу roles
+--7) Создать таблицу roles
 --- id. Serial  primary key,
 --- role_name. int, not null, unique
 
@@ -52,11 +52,11 @@ create table roles (
 	id serial primary key,
 	role_name int not null unique);
 	
---8)Поменять тип столба role_name ñ int íà varchar(30)
+--8) Поменять тип столба role_name ñ int íà varchar(30)
 alter table roles 
 alter role_name type varchar(30);
 
---9)Наполнить таблицу salary 20 строками:
+--9) Наполнить таблицу salary 20 строками:
 insert into roles (id, role_name)
 values (default, 'Senior Automation QA engineer');
 
@@ -76,7 +76,7 @@ create table roles_employee (
 	foreign key (role_id)
 		references roles (id));
 		
---11)Наполнить таблицу salary 40 строками:
+--11) Наполнить таблицу salary 40 строками:
 	
 insert into roles_employee (id, employee_id, role_id)
 values (default, 44, 14);
